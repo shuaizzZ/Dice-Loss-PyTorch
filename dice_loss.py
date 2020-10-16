e7990609a45416ae3c442fce638f91ebb17157f7
zhangshuai <zhangshuai@ainnovation.com>
2020-10-16 16:22:16 +0800

    weight_dice_loss

---
+++ dice_loss.py
@@ -0,0 +1,58 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+class DiceLoss(nn.Module):
+    """Dice Loss PyTorch
+        Created by: Zhang Shuai
+        Email: shuaizzz666@gmail.com
+        dice_loss = 1 - 2*p*t / (p^2 + t^2). p and t represent predict and target.
+    Args:
+        weight: An array of shape [C,]
+        predict: A float32 tensor of shape [N, C, *], for Semantic segmentation task is [N, C, H, W]
+        target: A int64 tensor of shape [N, *], for Semantic segmentation task is [N, H, W]
+    Return:
+        diceloss
+    """
+    def __init__(self, weight=None):
+        super(DiceLoss, self).__init__()
+        if weight is not None:
+            weight = torch.Tensor(weight)
+            self.weight = weight / torch.sum(weight) # Normalized weight
+        self.smooth = 1e-5
+
+    def forward(self, predict, target):
+        N, C = predict.size()[:2]
+        predict = predict.view(N, C, -1) # (N, C, *)
+        target = target.view(N, 1, -1) # (N, 1, *)
+
+        predict = F.softmax(predict, dim=1) # (N, C, *) ==> (N, C, *)
+        ## convert target(N, 1, *) into one hot vector (N, C, *)
+        target_onehot = torch.zeros(predict.size()).cuda()  # (N, 1, *) ==> (N, C, *)
+        target_onehot.scatter_(1, target, 1)  # (N, C, *)
+
+        intersection = torch.sum(predict * target_onehot, dim=2)  # (N, C)
+        union = torch.sum(predict.pow(2), dim=2) + torch.sum(target_onehot, dim=2)  # (N, C)
+        ## p^2 + t^2 >= 2*p*t, target_onehot^2 == target_onehot
+        dice_coef = (2 * intersection + self.smooth) / (union + self.smooth)  # (N, C)
+
+        if hasattr(self, 'weight'):
+            if self.weight.type() != predict.type():
+                self.weight = self.weight.type_as(predict)
+            dice_coef = torch.sum(dice_coef * self.weight, dim=1)  # (N, C)
+        dice_loss = 1 - torch.mean(dice_coef)  # 1
+
+        return dice_loss
+
+if __name__ == '__main__':
+    y_target = torch.Tensor([[0, 1], [1, 0]]).long().cuda(0)
+    y_predict = torch.Tensor([[[1.5, 1.0], [0.2, 1.6]],
+                           [[1.0, 1.0], [2.4, 0.3]]]
+                          ).cuda(0)
+
+    criterion = DiceLoss(weight=[1, 1])
+    loss = criterion(y_predict, y_target)
+    print(loss)
